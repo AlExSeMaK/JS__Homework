78a40360726f9351312661359c6530798db65090
aleksei <aleksejj219952@gmail.com>
2020-04-18 09:59:51 +0300

    homework

---
+++ Проверка возраста/age.js
@@ -0,0 +1,42 @@
+let users = [
+    {
+        login: "qwe",
+        age: 34,
+        city: "Москва",
+        favouriteLangs: ["php", "js"]
+    },
+    {
+        login: "asd",
+        age: 23,
+        city: "Москва",
+        favouriteLangs: ["python", "javascript"]
+    },
+    {
+        login: "rty56",
+        age: 58,
+        city: "Тверь",
+        favouriteLangs: ["java", "c"]
+    },
+    {
+        login: "rty56",
+        age: 58,
+        city: "Тверь",
+        favouriteLangs: ["java", "scala"]
+    },
+    {
+        login: "ght",
+        age: 45,
+        city: "Владивосток",
+        favouriteLangs: ["php", "ruby"]
+    },
+];
+
+function sort(arr, from, to=Infinity) {
+    for (let i = 0; i > arr.length; i++){
+        if (arr[i].age >= from && arr[i].age <= to){
+            return arr[i].login;
+        }
+    }
+}
+
+console.log(sort(users, 25, 66));
